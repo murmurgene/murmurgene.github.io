7baf97434860c30ef397940c2a7d6fd80a7a669d
murmurgene <murmurgene@gmail.com>
2026-01-21 15:50:22 +0000

    Deploying to main from @ murmurgene/GOELink@e675c17b395a7c0875229435eded493d1457e778 🚀

---
+++ GOELink/js/config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+    SUPABASE_URL: "https://kkwvvlilyyagndhoeotb.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtrd3Z2bGlseXlhZ25kaG9lb3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMzQ4NzksImV4cCI6MjA4MjkxMDg3OX0.rPdQ4wJmIR468FOIkIdSAXWc6PqjTkXIPTSEXSvQ2lk"
+};
